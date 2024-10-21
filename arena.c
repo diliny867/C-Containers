ef7d3f1d727f15b6ea27dae9d190e1ac9eb7166f
Kikorik <diliny867@gmail.com>
2024-10-21 16:48:52 +0300

    Implement memory arena

---
+++ arena.c
@@ -0,0 +1,70 @@
+#include "arena.h"
+
+
+#define _ARENA_CHUNK_DEFAULT_CAPACITY (16384)
+
+
+static arena_chunk_t* arena_chunk_new(size_t cap){
+    arena_chunk_t* chunk;
+    chunk = (arena_chunk_t*)malloc(sizeof(arena_chunk_t) + sizeof(uint8_t) * cap);
+    chunk->cap = cap;
+    chunk->size = 0;
+    chunk->next = NULL;
+    return chunk;
+}
+static void arena_chunk_free(arena_chunk_t* ch){
+    free(ch);
+}
+
+arena_t* arena_new(){
+    arena_t* a = (arena_t*)malloc(sizeof(arena_t));
+    a->start = NULL;
+    a->end = NULL;
+}
+void arena_free(arena_t* a){
+    if(a->start != NULL){
+        arena_chunk_t* ch = a->start;
+        arena_chunk_t* curr;
+        do{
+            curr = ch;
+            ch = ch->next;
+            arena_chunk_free(curr);
+        } while (ch != a->start);
+    }
+    free(a);
+}
+
+void* arena_alloc(arena_t* a, size_t size){
+    size_t alloc_size = _ARENA_CHUNK_DEFAULT_CAPACITY;
+    if(size > alloc_size){
+        alloc_size = ((size - 1) / _ARENA_CHUNK_DEFAULT_CAPACITY + 1) * _ARENA_CHUNK_DEFAULT_CAPACITY;
+    }
+
+    arena_chunk_t* ch = a->end;
+
+    if(ch == NULL){
+        a->end = arena_chunk_new(alloc_size);
+        ch = a->end;
+        a->start = ch;
+        ch->next = a->start;
+    }
+
+    if(ch->size + size <= ch->cap){
+        ch->size += size;
+        return ch->data + ch->size - size;
+    }
+
+    while(ch->next != a->end && ch->next->size + size > ch->next->cap){
+        ch = ch->next;
+    }
+
+    if(ch->next == a->end){
+        ch->next->next = arena_chunk_new(alloc_size);
+        ch = ch->next;
+        ch->next->next = a->start;
+    }
+
+    ch->next->size += size;
+    return ch->next->data + ch->next->size - size;
+}
+
---
+++ arena.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdint.h>
+
+
+typedef struct arena_chunk_t {
+    struct arena_chunk_t* next;
+    size_t size;
+    size_t cap;
+    uint8_t data[];
+} arena_chunk_t;
+
+typedef struct {
+    arena_chunk_t* start;
+    arena_chunk_t* end;
+} arena_t;
+
+
+arena_t* arena_new();
+void arena_free(arena_t* a);
+
+void* arena_alloc(arena_t* a, size_t size);
